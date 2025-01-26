9b17cc2deb68067e347d716a6666bde72f977836
GustavoYujiOkagawa <115659641+GustavoYujiOkagawa@users.noreply.github.com>
2025-01-26 01:08:26 +0000

    Primeiro commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,57 @@
 #include <stdio.h>
-
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    //Definindo as variáveis do projetos Cartas Super Tunfo
+    char CodigoCidade[100], nome[100];
+    float Populacao,area, pib;
+    int PontosTuristicos;
     
     // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+    printf("Insira as informações de cada cidade\n");
+
+    //Definindo a entrada do usuário
+    printf("Digite o código da cidade:\n");
+    scanf("%s", CodigoCidade);
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", nome);
+
+    printf("Informe a população da cidade:\n");
+    scanf("%f", &Populacao);
+
+    printf("Informe a área da cidade:\n");
+    scanf("%f", &area);
+
+    printf("Informe o PIB da cidade:\n");
+    scanf("%f", &pib);
+
+    printf("Informe o número de pontos turísticos da cidade:\n");
+    scanf("%d", &PontosTuristicos);
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Exibição dos Dados das Cartas
+    //Exiba os valores inseridos para cada atributo da cidade
+    printf("Código da cidade: %s ", CodigoCidade);
+    printf("Cidade: %s ", nome);
+    printf("População: %.3f ", Populacao);
+    printf("Área: %.3f km²", area);
+    printf("Pib: %.2f ", pib);
+    printf("Número de pontos turísticos: %d", PontosTuristicos);
 
     return 0;
 }
+
+
+/*Teste com esses dados
+    Populacão:  91.792 
+    Cidade: Avaré
+    Pib: R$ 2,82 bilhões
+    Pontos turísticos : 10
+    área 1.213.055
+*/
